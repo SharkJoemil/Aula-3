31fb023adff8d894c889e002bfbeb90a66f50741
SharkJoemil <shark04074@sharkcoders.pt>
2024-11-23 10:21:59 +0000

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -5,7 +5,7 @@ public partial class Player : Node
 {
 
 	//Definir a velocidade do personagem;
-	private Vector2 _speed = new Vector2(200,0);
+	private float _speed = 200.0f;
 
 	//Criar força de salto
 	private float _jumpvelocity = -600.0f;
@@ -22,7 +22,7 @@ public partial class Player : Node
 	//Function to check if player sprite is on floor
 	private bool isonFloor()
 	{
-		return _sprite.Position.Y >= 350;
+		return _sprite.Position.Y >= 250;
 	}
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -36,33 +36,45 @@ public partial class Player : Node
 
 	public override void _Process(double delta)
 	{
+
+		_velocity.X = 0;
 		//Atualiza a posição dp personagem com base na velocidade e tempo
-		_sprite.Position += _speed * (float)delta;
+		
+		//quando apertamos a seta para a esquerda andamos para a esquerda
+		if(Input.IsActionPressed("ui_new_action"))
+		{
+			_velocity.X = -_speed;
+		}
+		//quando apertamos a seta para a direita andamos para a direita
+		if(Input.IsActionPressed("ui_right"))
+		{
+			_velocity.X = _speed;
+		}
+		//Aplicaçao da gravidade ao jogador
+		_velocity.Y += gravity * (float)delta;
+		
 
 		//Salta se a tecla up for pressionada e tiver no chão
 		if(isonFloor() && Input.IsActionJustPressed("ui_up"))
 		{
 			_velocity.Y = _jumpvelocity;
 		}
-		
+		//Atualiza posiçao com base na velocidade
 		_sprite.Position += _velocity * (float)delta;
-
-		//Aplicação da gravidade ao jogador
-		_velocity.Y += gravity * (float)delta;
 
 		//Se o personagem ultrapassar a posição X de 1000
 		if (_sprite.Position.X > 1000)
 		{
 			//Reposiciona o personagem na posição inicial
-			_sprite.Position = new Vector2(-100, 350);
+			_sprite.Position = new Vector2(-100, 250);
 			_velocity = Vector2.Zero;
 		}
 
 		//Se o personagem estiver abaixo do chão
-		if (_sprite.Position.Y > 350)
+		if (_sprite.Position.Y > 250)
 		{
 			//o sprite fica com a velocidade nula e com a posição de 350 no eixo y
-			_sprite.Position = new Vector2(_sprite.Position.X, 350);
+			_sprite.Position = new Vector2(_sprite.Position.X, 250);
 			_velocity.Y = 0;
 		}
 		
